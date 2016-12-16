31298dab32e36b1267c0075b4cd809316d3f8f53
Roman Sheydvasser <s.roman@gmail.com>
2016-12-16 00:49:34 -0800

    add current rider location and pin

--- Transprt/RiderViewController.swift
+++ Transprt/RiderViewController.swift
@@ -8,22 +8,46 @@
 
 import UIKit
 import Parse
+import MapKit
 
-class RiderViewController: UIViewController {
+class RiderViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
 
+    var locationManager = CLLocationManager()
+    
     @IBOutlet weak var cancelTransBtn: UIButton!
+    @IBOutlet weak var map: MKMapView!
     
     @IBAction func logoutBtnPressed(_ sender: Any) {
         PFUser.logOutInBackground()
         self.performSegue(withIdentifier: "segueToLogin", sender: nil)
     }
     @IBAction func cancelTransBtnPressed(_ sender: Any) {
+
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = manager.location?.coordinate {
+            let center = CLLocationCoordinate2D(latitude: location.latitude, longitude: location.longitude)
+            let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
+            self.map.setRegion(region, animated: true)
+            
+            self.map.removeAnnotations(self.map.annotations)
+            let annotation = MKPointAnnotation()
+            annotation.coordinate = center
+            annotation.title = "Your Location"
+            self.map.addAnnotation(annotation)
+        }
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
     }
 
     override func didReceiveMemoryWarning() {
